ecd22b6d9253d7dd3f445eb88204ad96742ffee9
Kirill Zhuravlov <tjai89@mail.ru>
2015-06-20 00:17:40 +0300

    finish log in/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create 
   	@user = User.new(user_params)
   	if @user.save
+      log_in @user
   		flash[:success] = "Welcome to the Sample App!"
   		redirect_to @user
   	else 
@@ -20,7 +21,7 @@ class UsersController < ApplicationController
 	private
 		def user_params
 			params.require(:user).permit(:name, :email, :password,
-			:password_confirmation)
+			                             :password_confirmation)
 		end
 
 end
