fde604e77e91c30c54f4a83ec0d4a8f169a248a6
Kirill Zhuravlov <tjai89@mail.ru>
2015-07-20 06:40:44 +0300

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,8 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
 
   def show
-  	@user = User.find(params[:id])	
+  	@user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])	
   end
 
   def new
@@ -13,7 +14,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.paginate(page: params[:page]) 
+    @users = User.where(activated: true).paginate(page: params[:page]) 
   end
 
   def create 
@@ -57,14 +58,6 @@ class UsersController < ApplicationController
   # Confirm an admin user.
   def admin_user
     redirect_to(root_url) unless current_user.admin?
-  end
-
-  def logged_in_user 
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
   end  
 
   def correct_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 	attr_accessor :remember_token, :activation_token, :reset_token
 	before_save :downcase_email
 	before_create :create_activation_digest
+	has_many :microposts, dependent: :destroy
 
 	validates :name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -44,8 +45,7 @@ class User < ActiveRecord::Base
 
 	# Activates an account
 	def activate
-		update_attribute(:activated,     true)
-		update_attribute(:activated_at,  Time.zone.now)
+		update_columns(activated: true, activated_at: Time.zone.now)
 	end
 
 	#	Send activation email
@@ -56,8 +56,8 @@ class User < ActiveRecord::Base
 	# Sets the password reset attributes.
 	def create_reset_digest
 		self.reset_token = User.new_token
-		update_attribute(:reset_digest, User.digest(reset_token))
-		update_attribute(:reset_sent_at, Time.zone.now)
+		update_columns(reset_digest: 	User.digest(reset_token),
+									 reset_sent_at: Time.zone.now)
 	end
 
 	# Sends password reset email
@@ -70,6 +70,12 @@ class User < ActiveRecord::Base
 		reset_sent_at < 2.hours.ago
 	end
 
+	#Define a proto-feed.
+	#See "Following users" for the full implementation
+	def feed
+		Micropost.where("user_id =?", id)
+	end
+
 	private 
 	
 		# Converts email to all lower-case.
@@ -79,7 +85,7 @@ class User < ActiveRecord::Base
 
 		# Creates and assigns the activation token and digest.
 		def create_activation_digest
-			self.activation_token = User.new_token 
+			self.activation_token =  User.new_token 
 			self.activation_digest = User.digest(activation_token)
 		end
 end
